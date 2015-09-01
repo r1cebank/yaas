a36be9232a9d539912beb0538a9a930da250dc5c
Siyuan Gao <siyuangao@gmail.com>
2015-08-31 22:25:21 -0700

    ysql support for sql query from file

--- src/transform/yaas/sql/transform.js
+++ src/transform/yaas/sql/transform.js
@@ -13,6 +13,7 @@ import JPath            from 'jsonpath';
 import JsonFile         from 'jsonfile';
 import Path             from 'path';
 import _                from 'lodash';
+import MySQL            from 'mysql';
 
 //  Old require still using require
 var hash = require('json-hash');
@@ -24,30 +25,34 @@ function transform(req, file, version) {
     var sharedInstance = AppSingleton.getInstance();
 
     return new Promise((resolve, reject) => {
-        // If no param supplied, resolve
-        if(_.isEmpty(req)) {
-            sharedInstance.L.info(TAG, "no transform options provided.");
-            resolve({error: "no options provided"});
-        } else {
-            //  Needed the version number to enforce the files don't collide.
-            req.v = version;
-            var filename = Path.join(process.cwd(), sharedInstance.config.server.storage.processed, `${hash.digest(req)}.ysql`);
-            //  Needs to delete v to avoid confusions
-            delete req.v;
-            //  If exists in the system, don't bother processing it
-            //  Read this json file
-            JsonFile.readFile(file, function(err, obj) {
-                if(!err) {
-                    //  No error while reading ysql, continue to processing
-                    // TODO: write real processing here
-                    //  Open a new connection -> cache the connection -> query -> grab the results
-                    resolve(obj);
-                } else {
-                    sharedInstance.L.error(TAG, `error occurred: ${err.toString()}`);
-                    resolve({error : err.toString()});
-                }
-            });
-        }
+        req.v = version;
+        var filename = Path.join(process.cwd(), sharedInstance.config.server.storage.processed, `${hash.digest(req)}.ysql`);
+        //  Needs to delete v to avoid confusions
+        delete req.v;
+        //  If exists in the system, don't bother processing it
+        //  Read this json file
+        JsonFile.readFile(file, function(err, obj) {
+            if(!err) {
+                //  No error while reading ysql, continue to processing
+                // TODO: write real processing here
+                //  Open a new connection -> cache the connection -> query -> grab the results
+                var connection = MySQL.createConnection(obj.data.server);
+                //  Connect to MySQL
+                connection.connect();
+                connection.query(obj.data.query, (err, rows) => {
+                    if(err) {
+                        sharedInstance.L.error(TAG, `error occurred: ${err.toString()}`);
+                        reject(err);
+                    }
+                    resolve(rows);
+                    //  Close the connection
+                    connection.destroy();
+                });
+            } else {
+                sharedInstance.L.error(TAG, `error occurred: ${err.toString()}`);
+                resolve({error : err.toString()});
+            }
+        });
     });
 }
 
